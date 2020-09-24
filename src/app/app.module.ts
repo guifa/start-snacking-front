d54300d0a7e70147be4fb4eeb6f8e512167c0c48
Luiz Guilherme Ferreira de Alcantara <luizguilherme.alcantara@unimedfortaleza.com.br>
2020-09-24 20:57:54 -0300

    Add modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { IngredientListComponent } from './components/ingredient-list/ingredient-list.component';
+import { IngredientComponent } from './components/ingredient/ingredient.component';
+import { SnackIngredientComponent } from './components/snack-ingredient/snack-ingredient.component';
+import { SnackComponent } from './components/snack/snack.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    IngredientComponent,
+    SnackComponent,
+    IngredientListComponent,
+    SnackIngredientComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
